95d2101b03c0ab32c95e9ec5003053488764d731
João Gabriel <joaogabriel-al@hotmail.com>
2021-03-24 13:47:48 -0300

    Subindo Arquivos iniciais

---
+++ script.js
@@ -0,0 +1,17 @@
+function converter() {
+    let select = document.querySelector('#selecao').value
+    let temperatura = document.querySelector('#temperatura')
+    let mensagem = document.querySelector('.mensagem')
+
+    if (select == 'celsius') {
+        alert('C')
+        let temperaturaConvertida = (Number(temperatura.value) * 9/5) + 32
+        mensagem.innerHTML = `${temperatura.value} °C em Fahrenheit são ${temperaturaConvertida} °F`
+        
+    } else if (select == 'fahrenheit') {
+        alert('F')
+        let temperaturaConvertida = (Number(temperatura.value) - 32) * 5/9
+        mensagem.innerHTML = `${temperatura.value} °F em Celsius são ${temperaturaConvertida} °C`
+    }
+
+}
